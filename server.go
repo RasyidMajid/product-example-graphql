707191a724bedf13c2ba054771e7a203ee78addd
Rasyid Maulid Majid <rasyid.majid@misteraladin.com>
2023-01-30 18:21:41 +0700

    add opentracing and jaeger

--- server.go
+++ server.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"github.com/99designs/gqlgen/graphql/handler"
 	"github.com/99designs/gqlgen/graphql/playground"
+	"github.com/opentracing/opentracing-go"
 	"log"
 	"net/http"
 	"os"
@@ -11,6 +12,7 @@ import (
 	"product-service-graphql/src/infrastructure/controller"
 	"product-service-graphql/src/infrastructure/generated"
 	container "product-service-graphql/src/shared/di"
+	"product-service-graphql/src/shared/tracing"
 	productUsecase "product-service-graphql/src/usecase/products"
 )
 
@@ -29,6 +31,10 @@ func main() {
 
 	http.Handle("/", playground.Handler("GraphQL playground", "/query"))
 	http.Handle("/query", srv)
+
+	tracer, closer := tracing.Init("Product-service")
+	defer closer.Close()
+	opentracing.SetGlobalTracer(tracer)
 
 	log.Printf("connect to http://localhost:%s/ for GraphQL playground", port)
 	log.Fatal(http.ListenAndServe(":"+port, nil))
